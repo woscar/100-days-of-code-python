9b6dfa9d6d75296a4807e7f3d266eb431b8a060e
Wellington Oscar <w_oscar@yahoo.com.br>
2022-12-13 20:09:07 +0100

    Fixing code
    Fixing code

--- day-03/main.py
+++ day-03/main.py
@@ -20,7 +20,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Treasure Island.")
+print("Hey, Captain Woscar! Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
@@ -31,7 +31,7 @@ choice1 = input('You\'re at a crossroad. Where do you want to go? Type "left" or
 if choice1 == "left":
   #continue in the game
   choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
-  if choice2 == "swim":
+  if choice2 == "wait":
   #continue in the game
     choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
     if choice3 == "red":
@@ -42,5 +42,7 @@ if choice1 == "left":
       print("You win.")
     else:
       print("Game over")
+  else:
+    print("You got attacked by a crazy shark, game over")
 else:
   print ("Fall into a hole. Game Over.")
